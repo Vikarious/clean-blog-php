88f40a3620bea372785a5dd58efd7611a2cc6774
Vikarious <fialaire.arnaud@free.fr>
2022-03-18 13:30:21 +0100

    added 4 models

--- post.php
+++ post.php
@@ -1,23 +1,21 @@
-<?php include("model/post1.model.php"); ?>
+        <?php include("model/post1.model.php"); ?>
+        
 
+        <?php include("nav.inc.php"); ?>
+        
+        <?php include("header.inc.php"); ?>
 
-<?php include("nav.inc.php"); ?>
-
-<?php include("header.inc.php"); ?>
-
-<!-- Post Content-->
-<article class="mb-4">
-    <div class="container px-4 px-lg-5">
-        <div class="row gx-4 gx-lg-5 justify-content-center">
-            <div class="col-md-10 col-lg-8 col-xl-7">
-                <p>
-                    <?php echo $content; ?>
-                </p>
+        <!-- Post Content-->
+        <article class="mb-4">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-md-10 col-lg-8 col-xl-7">
+                        <p><?php echo $content; ?></p>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</article>
-
-<?php include("footer.inc.php"); ?>
-
+        </article>
+        
+        <?php include("footer.inc.php"); ?>
+        
 
