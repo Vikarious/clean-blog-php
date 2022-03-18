7b2ab7ee1acae4530ae3bca7a627e75cbdcda010
Vikarious <fialaire.arnaud@free.fr>
2022-03-18 09:37:39 +0100

    init

---
+++ clean/model/post1.model.php
@@ -0,0 +1,4 @@
+<?php
+$title = "Lorem Ipsum";
+$subtitle = "un peu de lorem ipsium";
+$content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione quos autem id numquam aliquam voluptates quae facere aut. Iure laudantium non iusto corrupti enim illum odit magnam dolore quo libero?";
---
+++ clean/post.php
@@ -0,0 +1,24 @@
+<?php include("model/post1.model.php"); ?>
+<!-- Navigation-->
+<?php include("nav.inc.php"); ?>
+<!-- Page Header-->
+<?php include("header.inc.php"); ?>
+<!-- Post Content-->
+<article class="mb-4">
+    <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <p>
+                    <?php echo $content; ?>
+                <p>
+                    Placeholder text by
+                    <a href="http://spaceipsum.com/">Space Ipsum</a>
+                    &middot; Images by
+                    <a href="https://www.flickr.com/photos/nasacommons/">NASA on The Commons</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</article>
+<!-- Footer-->
+<?php include("footer.inc.php"); ?>
