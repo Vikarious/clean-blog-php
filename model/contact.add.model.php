557595969c38615753b694b0caa34491d5d740e0
Vikarious <fialaire.arnaud@free.fr>
2022-03-25 10:41:00 +0100

    asdf

--- model/contact.add.model.php
+++ model/contact.add.model.php
@@ -4,13 +4,40 @@ include("model/pdo.inc.php");
 
 try {
     // var_dump($_POST);
-    $query = "INSERT INTO blog_contacts
-    (contact_name, contact_email, contact_phone, contact_message)
-    VALUES
-    ( '" . addslashes($_POST["name"]) . "', '" . $_POST["email"] . "',
-    '" . $_POST["phone"] . "', '" . addslashes($_POST["message"]) . "' )";
+    // $query = "INSERT INTO blog_contacts(
+    //     contact_name,
+    //     contact_email,
+    //     contact_phone,
+    //     contact_message
+    // )
+    // VALUES(
+    //     '" . addslashes($_POST["name"]) . "',
+    //     '" . $_POST["email"] . "',
+    //     '" . $_POST["phone"] . "',
+    //     '" . addslashes($_POST["message"]) . "'
+    // )";
+    // $req = $pdo->exec($query);
 
-    $req = $pdo->exec($query);
+    $query = "INSERT INTO blog_contacts(
+        contact_name,
+        contact_email,
+        contact_phone,
+        contact_message
+    )
+    VALUES(
+        :contact_name,
+        :contact_email,
+        :contact_phone,
+        :contact_message
+
+    )";
+    $curseur = $pdo->prepare($query);
+    $curseur->bindValue(':contact_name', $_POST["name"], PDO::PARAM_STR);
+    $curseur->bindValue(':contact_email', $_POST["email"], PDO::PARAM_STR);
+    $curseur->bindValue(':contact_phone', $_POST["phone"], PDO::PARAM_STR);
+    $curseur->bindValue(':contact_message', $_POST["message"], PDO::PARAM_STR);
+
+    $curseur->execute();
 
     // die($query);
 } catch (Exception $e) {
